1280e4c6a2b680e6dea84de7bdbf2397c6829ff4
ffon <poty1900@hotmail.com>
2017-06-23 15:00:53 +0700

    Update push.php

--- push.php
+++ push.php
@@ -41,12 +41,25 @@
 // curl_close ($ch);
 // echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 //-----------------------------------------------------------------------------
+$proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
+$proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
+$url = 'https://api.line.me/v2/bot/message/reply';
 var bot = LINEBot.create({
   channelID: '1521137457',
   channelSecret: '2016f3f7fb001c7f38154a3fe3f3202c',
   channelToken: 'Wrmy2j+qSD5kpeDpMTKc5UYXWSSA9h1wZ51d6hkzbhingG2bI0EJJtNC97coCiY/QPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctf3wF09jCF5XBhXzv8Y8+ESj41YUNx13e3fUjRj6cUIQdB04t89/1O/w1cDnyilFU='
 })
 bot.pushTextMessage('U7de80d0a2ceea863e831375badd2eb55', 'hello!');
+   $ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
 
 
 
