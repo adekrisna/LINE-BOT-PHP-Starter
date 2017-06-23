7daa6a22cf4aef99f72a67d4412513a864ae533e
ffon <poty1900@hotmail.com>
2017-06-23 13:43:36 +0700

    Update push.php

--- push.php
+++ push.php
@@ -1,4 +1,4 @@
-include ('line-bot-api/line-bot.php');
+include ('LINE-BOT-PHP-Starter/line-bot.php');
 
 $channelSecret = '2016f3f7fb001c7f38154a3fe3f3202c';
 $access_token  = 'Wrmy2j+qSD5kpeDpMTKc5UYXWSSA9h1wZ51d6hkzbhingG2bI0EJJtNC97coCiY/QPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctf3wF09jCF5XBhXzv8Y8+ESj41YUNx13e3fUjRj6cUIQdB04t89/1O/w1cDnyilFU=';
