80a5e6f8e7d714a27f03b026f67ed60f4dbf4e14
ffon <poty1900@hotmail.com>
2017-06-23 14:57:45 +0700

    Update push.php

--- push.php
+++ push.php
@@ -40,6 +40,14 @@
 // $result = curl_exec($ch);
 // curl_close ($ch);
 // echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+//-----------------------------------------------------------------------------
+var bot = LINEBot.create({
+  channelID: '1521137457',
+  channelSecret: '2016f3f7fb001c7f38154a3fe3f3202c',
+  channelToken: 'Wrmy2j+qSD5kpeDpMTKc5UYXWSSA9h1wZ51d6hkzbhingG2bI0EJJtNC97coCiY/QPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctf3wF09jCF5XBhXzv8Y8+ESj41YUNx13e3fUjRj6cUIQdB04t89/1O/w1cDnyilFU='
+})
 bot.pushTextMessage('U7de80d0a2ceea863e831375badd2eb55', 'hello!');
+
+
 
 ?>
