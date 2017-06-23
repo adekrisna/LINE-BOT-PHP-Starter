052bbcb4f01ab8fbbb5b9e3bec5ba88845fb9b5a
ffon <poty1900@hotmail.com>
2017-06-23 11:15:38 +0700

    Update push.php

--- push.php
+++ push.php
@@ -1,42 +1,36 @@
 <?php
-$proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
-$proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
+// $proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
+// $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
  
-$strAccessToken = "Wrmy2j+qSD5kpeDpMTKc5UYXWSSA9h1wZ51d6hkzbhingG2bI0EJJtNC97coCiY/QPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctf3wF09jCF5XBhXzv8Y8+ESj41YUNx13e3fUjRj6cUIQdB04t89/1O/w1cDnyilFU=";
- 
-$strUrl = "https://api.line.me/v2/bot/message/push";
- 
-// $arrHeader = array();
-// $arrHeader[] = "Content-Type: application/json";
-// $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
- 
-// $arrPostData = array();
-// $arrPostData['to'] = "fozenffon";
-// $arrPostData['messages'][0]['type'] = "text";
-// $arrPostData['messages'][0]['text'] = "Test Push Message";
+include ('line-bot-api/php/line-bot.php');
 
-//-----------------------------------------
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('Wrmy2j+qSD5kpeDpMTKc5UYXWSSA9h1wZ51d6hkzbhingG2bI0EJJtNC97coCiY/QPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctf3wF09jCF5XBhXzv8Y8+ESj41YUNx13e3fUjRj6cUIQdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '2016f3f7fb001c7f38154a3fe3f3202c']);
+$channelSecret = '551ec4feee0.....43cff0';
+$access_token  = '2og9ogezC8k.......5ZUEQQdB04t89/1O/w1cDnyilFU=';
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->pushMessage('fozenffon', $textMessageBuilder);
+$bot = new BOT_API($channelSecret, $access_token);
+	
+$bot->sendMessageNew('[Your userId / User Id]', 'Hello World !!');
 
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+if ($bot->isSuccess()) {
+	echo 'Succeeded!';
+	exit();
+}
 
-//-------------------------------------
- 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$strUrl);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $httpClient);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($textMessageBuilder));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_PROXY, $proxy);
-curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-$result = curl_exec($ch);
-curl_close ($ch);
-echo "ok";
+// Failed
+echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+exit();
+
+// $ch = curl_init();
+// curl_setopt($ch, CURLOPT_URL,$strUrl);
+// curl_setopt($ch, CURLOPT_HEADER, false);
+// curl_setopt($ch, CURLOPT_POST, true);
+// curl_setopt($ch, CURLOPT_HTTPHEADER, $httpClient);
+// curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($textMessageBuilder));
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+// curl_setopt($ch, CURLOPT_PROXY, $proxy);
+// curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+// $result = curl_exec($ch);
+// curl_close ($ch);
+// echo "ok";
 ?>
