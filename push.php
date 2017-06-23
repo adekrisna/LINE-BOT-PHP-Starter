0b864e3427b88e3c4813f872e67220377df3d19e
ffon <poty1900@hotmail.com>
2017-06-23 11:17:31 +0700

    Update push.php

--- push.php
+++ push.php
@@ -4,12 +4,12 @@
  
 include ('line-bot-api/php/line-bot.php');
 
-$channelSecret = '551ec4feee0.....43cff0';
-$access_token  = '2og9ogezC8k.......5ZUEQQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '2016f3f7fb001c7f38154a3fe3f3202c';
+$access_token  = 'Wrmy2j+qSD5kpeDpMTKc5UYXWSSA9h1wZ51d6hkzbhingG2bI0EJJtNC97coCiY/QPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctf3wF09jCF5XBhXzv8Y8+ESj41YUNx13e3fUjRj6cUIQdB04t89/1O/w1cDnyilFU=';
 
 $bot = new BOT_API($channelSecret, $access_token);
 	
-$bot->sendMessageNew('[Your userId / User Id]', 'Hello World !!');
+$bot->sendMessageNew('fozenffon', 'Hello World !!');
 
 if ($bot->isSuccess()) {
 	echo 'Succeeded!';
