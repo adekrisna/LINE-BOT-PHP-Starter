65ad2d38fec6599029c2210267d35f8f184430f8
ffon <poty1900@hotmail.com>
2017-06-23 14:42:15 +0700

    Update push.php

--- push.php
+++ push.php
@@ -1,39 +1,48 @@
-<!-- 
-<?php
+
+
  
-$strAccessToken = "Wrmy2j+qSD5kpeDpMTKc5UYXWSSA9h1wZ51d6hkzbhingG2bI0EJJtNC97coCiY/QPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctf3wF09jCF5XBhXzv8Y8+ESj41YUNx13e3fUjRj6cUIQdB04t89/1O/w1cDnyilFU=";
+// $strAccessToken = "Wrmy2j+qSD5kpeDpMTKc5UYXWSSA9h1wZ51d6hkzbhingG2bI0EJJtNC97coCiY/QPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctf3wF09jCF5XBhXzv8Y8+ESj41YUNx13e3fUjRj6cUIQdB04t89/1O/w1cDnyilFU=";
  
-$strUrl = "https://api.line.me/v2/bot/message/push";
+// $strUrl = "https://api.line.me/v2/bot/message/push";
  
-$arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+// $arrHeader = array();
+// $arrHeader[] = "Content-Type: application/json";
+// $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
-$arrPostData = array();
-$arrPostData['to'] = "U7de80d0a2ceea863e831375badd2eb55";
-$arrPostData['messages'][0]['type'] = "text";
-$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
+// $arrPostData = array();
+// $arrPostData['to'] = "U7de80d0a2ceea863e831375badd2eb55";
+// $arrPostData['messages'][0]['type'] = "text";
+// $arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
  
  
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$strUrl);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$result = curl_exec($ch);
-curl_close ($ch);
+// $ch = curl_init();
+// curl_setopt($ch, CURLOPT_URL,$strUrl);
+// curl_setopt($ch, CURLOPT_HEADER, false);
+// curl_setopt($ch, CURLOPT_POST, true);
+// curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+// curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+// $result = curl_exec($ch);
+// curl_close ($ch);
  
-?> -->
+
 
 <?php
+
+$strUrl = "https://api.line.me/v2/bot/message/push";
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('Wrmy2j+qSD5kpeDpMTKc5UYXWSSA9h1wZ51d6hkzbhingG2bI0EJJtNC97coCiY/QPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctf3wF09jCF5XBhXzv8Y8+ESj41YUNx13e3fUjRj6cUIQdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '2016f3f7fb001c7f38154a3fe3f3202c']);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
 $response = $bot->pushMessage('U7de80d0a2ceea863e831375badd2eb55', $textMessageBuilder);
-
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+$result = curl_exec($ch);
+curl_close ($ch);
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
 ?>
