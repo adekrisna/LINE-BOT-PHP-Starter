fab8ec71b7b606b54fe07ba839beda4b13b95ef8
ffon <poty1900@hotmail.com>
2017-06-23 17:26:56 +0700

    Update push.php

--- push.php
+++ push.php
@@ -26,6 +26,7 @@
 // curl_close ($ch);
 
 //-------------------------------------------------------------------------------
+$url = 'https://api.line.me/v2/bot/message/reply';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('Wrmy2j+qSD5kpeDpMTKc5UYXWSSA9h1wZ51d6hkzbhingG2bI0EJJtNC97coCiY/QPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctf3wF09jCF5XBhXzv8Y8+ESj41YUNx13e3fUjRj6cUIQdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '2016f3f7fb001c7f38154a3fe3f3202c']);
@@ -33,6 +34,16 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '2016f3f7fb001c7f38154
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
 $response = $bot->pushMessage('U7de80d0a2ceea863e831375badd2eb55', $textMessageBuilder);
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+$ch = curl_init($url);
+			
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
 
 
 
