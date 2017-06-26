0131082e5baa2fb779a3faac3e23e3f47a60b1ab
ffon <poty1900@hotmail.com>
2017-06-26 12:26:42 +0700

    Update testlineapi.php

--- testlineapi.php
+++ testlineapi.php
@@ -59,7 +59,7 @@
 if ($_POST) { 
 
 //Setting
-$lineapis = array(0 => "XQLflZY084crGCIAwYbkDEjxVA8D4jcGScx5OEkxAeY", 1=> "1V6jR6xRWaUh0uXSu309vfLyJxllSPLgn0DZnRJKiAt");
+//$lineapis = array(0 => "XQLflZY084crGCIAwYbkDEjxVA8D4jcGScx5OEkxAeY", 1=> "1V6jR6xRWaUh0uXSu309vfLyJxllSPLgn0DZnRJKiAt");
 $lineapi = "XQLflZY084crGCIAwYbkDEjxVA8D4jcGScx5OEkxAeY";
 $mms =  trim($_POST['textArea']);
 //foreach($lineapis as $key => $lineapi){ 
