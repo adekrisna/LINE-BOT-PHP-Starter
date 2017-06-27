5d09f5adc1fea6a79866b5981539f124c624c8d0
ffon <poty1900@hotmail.com>
2017-06-27 11:33:58 +0700

    Update testPush.php

--- testPush.php
+++ testPush.php
@@ -1,31 +1,35 @@
-<?php 
-$strAccessToken = "QQ4FDBydERg5R34tFiff7M+OOuRNzYKDA/btJh4Whsgl0ztKiDparY2v3TyaoL1LQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctP74gTqe5/G/kLHS2Ixe3w0jsLIaN0guHlHI+3q9c9ZQdB04t89/1O/w1cDnyilFU="; 
-$mids = array(0=>'U7de80d0a2ceea863e831375badd2eb55', 1=>'Ub5fea2ff169cba24b2179fd33e59e454'); //fon,oil  
-foreach($mids as $key => $mid){         
-        $messages=[ 
-                    "type" => "text",     
-                    "text" => "prove of concept: multiple user"  
-        ]; 
-  
-        $post_data = [ 
-            "to" => $mid, 
-            "messages" => [$messages]  
-        ]; 
-  
-        $header = array( 
-            'Content-Type: application/json', 
-            'Authorization: Bearer ' . $strAccessToken 
-        ); 
-        $url = 'https://api.line.me/v2/bot/message/push'; 
-        $ch = curl_init($url); 
-        curl_setopt($ch, CURLOPT_POST, true); 
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST'); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data)); 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header); 
-  
-        $result = curl_exec($ch); 
-        curl_close($ch);} 
+<?php
+$proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
+$proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';  
+$strAccessToken = "QQ4FDBydERg5R34tFiff7M+OOuRNzYKDA/btJh4Whsgl0ztKiDparY2v3TyaoL1LQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctP74gTqe5/G/kLHS2Ixe3w0jsLIaN0guHlHI+3q9c9ZQdB04t89/1O/w1cDnyilFU=";
+$mids = array(0=>'U7de80d0a2ceea863e831375badd2eb55'); 
+foreach($mids as $key => $mid){        
+        $messages = [
+            "type" => "text",
+            "text" => 'Hello DX'
+        ];
+ 
+        $post_data = [
+            "to" => $mid,
+            "messages" => [$messages]
+        ];
+ 
+        $header = array(
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $strAccessToken
+        );
+        $url = 'https://api.line.me/v2/bot/message/push';
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_PROXY, $proxy);
+        curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+        $result = curl_exec($ch);
+        curl_close($ch);
+}
 
-  
+ 
 ?>
