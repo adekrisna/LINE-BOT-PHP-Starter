62ce6072caf0e0634b6a67f2f0e880592fa86e7c
ffon <poty1900@hotmail.com>
2017-06-27 18:15:22 +0700

    Update testlineapi.php

--- testlineapi.php
+++ testlineapi.php
@@ -62,7 +62,7 @@ $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
 if ($_POST) { 
 
 //Setting
-$lineapis = array(0 => "htGDXAYmHtW07XQwaCVU4FMFoLCrQwypDTXhQgSaSSo", 1=> "dm43sIVngCBt9oD1ZkQDtW5YWmuYpjCTeMcSLJUJDhS");
+$lineapis = array(0 => "mtBNqNUTljJzkrsjtubrH5hPvt4Lq93WXYJUPVLTNBw", 1=> "dm43sIVngCBt9oD1ZkQDtW5YWmuYpjCTeMcSLJUJDhS");
 //$lineapi = "49L0CDQLq1swOl8cElkaxpSbO3WSKl2rS2evcIdlgex";
 $mms =  trim($_POST['textArea']);
 foreach($lineapis as $key => $lineapi){ 
