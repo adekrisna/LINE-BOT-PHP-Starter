30ce1ef9ecc7c2e096af2c01446e33334edf5d99
ffon <poty1900@hotmail.com>
2017-06-29 16:57:40 +0700

    Update testPush.php

--- testPush.php
+++ testPush.php
@@ -2,7 +2,7 @@
 $proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
 $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';  
 $strAccessToken = "QQ4FDBydERg5R34tFiff7M+OOuRNzYKDA/btJh4Whsgl0ztKiDparY2v3TyaoL1LQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctP74gTqe5/G/kLHS2Ixe3w0jsLIaN0guHlHI+3q9c9ZQdB04t89/1O/w1cDnyilFU=";
-$mids = array(0=>'U7de80d0a2ceea863e831375badd2eb55','Ub5fea2ff169cba24b2179fd33e59e454'); 
+$mids = array(0=>'U7de80d0a2ceea863e831375badd2eb55'); 
 foreach($mids as $key => $mid){        
         $messages = [
             "type" => "text",
