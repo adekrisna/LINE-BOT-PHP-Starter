f2cd22d443af5971b8767e2a3ec626a9b7c60630
ffon <poty1900@hotmail.com>
2017-06-29 15:42:03 +0700

    Update pushMsg2.php

--- pushMsg2.php
+++ pushMsg2.php
@@ -61,7 +61,7 @@
                                 <div class="modal-content">
                                     <div class="modal-header">
                                         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                        <h4 class="modal-title" id="myModalLabel">Member</h4>
+                                        <h4 class="modal-title" id="myModalLabel">111</h4>
                                     </div>
                                     <div class="container">
                                          
