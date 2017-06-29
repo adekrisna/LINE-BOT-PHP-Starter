f9a0ea791a600511532265fb25f9504701d8c363
ffon <poty1900@hotmail.com>
2017-06-29 13:55:48 +0700

    Update pushMsg2.php

--- pushMsg2.php
+++ pushMsg2.php
@@ -107,10 +107,10 @@ $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
 $strAccessToken = 'QQ4FDBydERg5R34tFiff7M+OOuRNzYKDA/btJh4Whsgl0ztKiDparY2v3TyaoL1LQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctP74gTqe5/G/kLHS2Ixe3w0jsLIaN0guHlHI+3q9c9ZQdB04t89/1O/w1cDnyilFU=';            
 $text = $_POST['textArea'];
 $midUser = $_POST['mid'];  
-$tokenLine = $_POST['tokenLine'];
-
+$tokenLine11 = $_POST['tokenLine'];
+var_dump($tokenLine11) ;
 //$strAccessToken1 = $tokenLine;
-var_dump($tokenLine) ;
+
 //$mids = array($midUser); 
 foreach($midUser as $key => $mid){     
         
