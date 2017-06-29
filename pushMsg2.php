19842625c435f167fef8bb960ddcb3c3dea189c3
ffon <poty1900@hotmail.com>
2017-06-29 13:17:41 +0700

    Update pushMsg2.php

--- pushMsg2.php
+++ pushMsg2.php
@@ -54,7 +54,7 @@
                     </div>
                     <!--Modal-->
     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-          <form method="post" name="n">
+          <form method="post" name="n" id="formcon">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -64,16 +64,16 @@
                 <div class="container">
                   
                         <div class="checkbox">
-                            <label><input type="checkbox" value="Ub5fea2ff169cba24b2179fd33e59e454" id="test1" name="mid">oil</label>
+                            <label><input type="checkbox" value="Ub5fea2ff169cba24b2179fd33e59e454" id="test1" name="mid1">oil</label>
                         </div>
                         <div class="checkbox">
-                            <label><input type="checkbox" value="U7de80d0a2ceea863e831375badd2eb55" id="test2" name="mid">ffon</label>
+                            <label><input type="checkbox" value="U7de80d0a2ceea863e831375badd2eb55" id="test2" name="mid2">ffon</label>
                         </div>
                         
                     
                 </div>
                 <div class="modal-footer">
-                    <button type="submit" class="btn btn-primary" name="submit">Summit</button>
+                    <button type="submit" class="btn btn-primary" form="formcon" name="submit">Summit</button>
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                 </div>
             </div>
@@ -107,9 +107,10 @@ $proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
 $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';  
 $text = $_POST['textArea'];
 $midUser = array($_POST['mid']);
+    $midUser2 = array($_POST['formcon']);
 //$lineMaster = $_POST['lineMaster'];
 
-var_dump($midUser) ;
+var_dump($midUser2) ;
 //var_dump($lineMaster);
 
 
