1c1d2d063fbc41f8c64a5aed7fb99de978f82ef1
ffon <poty1900@hotmail.com>
2017-06-29 12:39:29 +0700

    Update pushMsg2.php

--- pushMsg2.php
+++ pushMsg2.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="th">
+
 <head>
     <title>Push Messages</title>
-  <meta charset="utf-8">  
-  <meta name="viewport" content="width=device-width, initial-scale=1">  
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 
     <style>
         .head-form h1 {
@@ -26,24 +28,43 @@
 </head>
 
 <body>
+
+
     <div class="container">
         <div class="row">
             <div class="col-xs-12 head-form">
                 <h1 align = "center">Push Massages</h1>
             </div>
+            <form type="post"><div class="form-group" align="center">
+           <button type="button" class="btn btn-primary" value="QQ4FDBydERg5R34tFiff7M+OOuRNzYKDA/btJh4Whsgl0ztKiDparY2v3TyaoL1LQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctP74gTqe5/G/kLHS2Ixe3w0jsLIaN0guHlHI+3q9c9ZQdB04t89/1O/w1cDnyilFU=" name="lineMaster"> Line@ffon</button>
+           <button type="button" class="btn btn-primary" value="xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=" name="lineMaster"> Line@oil</button>
+            </div><form>
+
             <div class="col-md-8 col-md-offset-2">
                 <form method="post">
                     <div class="form-group">
                         <label>Text</label>
                         <textarea class="form-control" rows="8" id="textArea" name="textArea"></textarea>
                     </div>
-                     <!--buttonMember-->
+                    <!--buttonMember-->
                     <div class="form-group" align="center">
                         <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal" style="margin-top:30px;margin-bottom:20px;">
                         MEMBER
                         </button>
-                    </div>                   
-                    <!--Modal-->
+                    </div>
+                    <!--submitCancel-->
+                    <div class="button-sc text-center">
+                        <button type="cancel" class="btn btn-default" style="margin-right:10px;">Cancel</button>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+<!--Modal-->
     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -57,28 +78,19 @@
                             <label><input type="checkbox" value="U7de80d0a2ceea863e831375badd2eb55" name="mid">ffon</label>
                         </div>
                         <div class="checkbox">
-                            <label><input type="checkbox" value="Ub5fea2ff169cba24b2179fd33e59e454" name="mid">oil</label>
+                            <label><input type="checkbox" value="Ub5fea2ff169cba24b2179fd33e59e454"name="mid">oil</label>
                         </div>
                     </form>
                 </div>
                 <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">close</button>
                     <button type="submit" class="btn btn-primary" name="submit">Summit</button>
-                 <button type="cancel" class="btn btn-default" data-dismiss="modal">Close</button> 
+                    
                 </div>
             </div>
         </div>
     </div>
-                    
-                    <!--submitCancel-->
-                    <div class="button-sc text-center">
-                        <button type="submit" class="btn btn-primary" name="submit">Submit</button>
-                        <button type="cancel" class="btn btn-default" style="margin-right:10px;">Cancel</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-    
+
     <script>
         $('#myModal').on('shown.bs.modal', function () {
             $('#myInput').focus()
@@ -86,11 +98,17 @@
     </script>
 
 
+
 <?php
 $proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
 $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';  
 $text = $_POST['textArea'];
 $midUser = $_POST['mid'];
+$lineMaster = $_POST['lineMaster'];
+
+var_dump($lineMaster) ;
+var_dump($midUser);
+
 $strAccessToken = "QQ4FDBydERg5R34tFiff7M+OOuRNzYKDA/btJh4Whsgl0ztKiDparY2v3TyaoL1LQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctP74gTqe5/G/kLHS2Ixe3w0jsLIaN0guHlHI+3q9c9ZQdB04t89/1O/w1cDnyilFU=";
 $mids = array($midUser); 
 foreach($mids as $key => $mid){        
