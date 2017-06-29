55d00241cddb0d38b700c346a0d51bb1e37c0ed0
ffon <poty1900@hotmail.com>
2017-06-29 12:12:33 +0700

    Update pushMsg2.php

--- pushMsg2.php
+++ pushMsg2.php
@@ -63,7 +63,8 @@
                 </div>
                 <div class="modal-footer">
                     <button type="submit" class="btn btn-primary" name="submit">Summit</button>
-                    <button type="cancel" class="btn btn-default" data-dismiss="modal">close</button>
+                     <button type="cancel" class="btn btn-default" style="margin-right:10px;">Close</button>
+<!--                     <button type="cancel" class="btn btn-default" data-dismiss="modal">close</button> -->
                 </div>
             </div>
         </div>
