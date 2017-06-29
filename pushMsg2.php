d77d53d6a63f5668631033503f2ca8d5de923f4b
ffon <poty1900@hotmail.com>
2017-06-29 16:52:44 +0700

    Update pushMsg2.php

--- pushMsg2.php
+++ pushMsg2.php
@@ -95,66 +95,24 @@
 
 
 <?php
-// $proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
-// $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80'; 
-// $strAccessToken = 'QQ4FDBydERg5R34tFiff7M+OOuRNzYKDA/btJh4Whsgl0ztKiDparY2v3TyaoL1LQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctP74gTqe5/G/kLHS2Ixe3w0jsLIaN0guHlHI+3q9c9ZQdB04t89/1O/w1cDnyilFU=';            
-
-// $text = $_POST['textArea'];
-// $midUser = $_POST['mid'];  
-// //$strAccessToken = $_POST['tokenLine'];
-
-//     echo("this is toke : \n");
-// var_dump($strAccessToken) ;
-
-//     echo("this is mid : \n");
-// var_dump($midUser) ;
-
-//     echo("this is text : \n");
-// var_dump($text) ;
-
-    
-
-
-// //$mids = array($midUser); 
-// foreach($midUser as $key => $mid){     
-        
-//         $messages = [
-//             "type" => "text",
-//             "text" => $text
-//         ];
- 
-//         $post_data = [
-//             "to" => $mid,
-//             "messages" => [$messages]
-//         ];
-
-    
-//         $header = array(
-//             'Content-Type: application/json',
-//             'Authorization: Bearer ' . $strAccessToken
-//         );
-//         $url = 'https://api.line.me/v2/bot/message/push';
-//         $ch = curl_init($url);
-//         curl_setopt($ch, CURLOPT_POST, true);
-//         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-//         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
-//         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-//         curl_setopt($ch, CURLOPT_PROXY, $proxy);
-//         curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-//         $result = curl_exec($ch);
-//     echo("this is result : \n");
-//         var_dump($result);
-//         curl_close($ch);
-// }
- 
 $proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
-$proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';  
+$proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80'; 
+$strAccessToken = 'QQ4FDBydERg5R34tFiff7M+OOuRNzYKDA/btJh4Whsgl0ztKiDparY2v3TyaoL1LQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctP74gTqe5/G/kLHS2Ixe3w0jsLIaN0guHlHI+3q9c9ZQdB04t89/1O/w1cDnyilFU=';            
+
 $text = $_POST['textArea'];
-$midUser = $_POST['mid'];
-$strAccessToken = "QQ4FDBydERg5R34tFiff7M+OOuRNzYKDA/btJh4Whsgl0ztKiDparY2v3TyaoL1LQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctP74gTqe5/G/kLHS2Ixe3w0jsLIaN0guHlHI+3q9c9ZQdB04t89/1O/w1cDnyilFU=";
+$midUser = $_POST['mid'];  
+//$strAccessToken = $_POST['tokenLine'];
 
+    echo("this is toke : \n");
+var_dump($strAccessToken) ;
 
+    echo("this is mid : \n");
+var_dump($midUser) ;
+
+    echo("this is text : \n");
+var_dump($text) ;
+
+//$mids = array($midUser); 
 foreach($midUser as $key => $mid){        
         $messages = [
             "type" => "text",
@@ -182,6 +140,10 @@ foreach($midUser as $key => $mid){
         $result = curl_exec($ch);
         curl_close($ch);
 }
+ 
+
+
+
  
  ?>
 </body>
