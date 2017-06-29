cc81640ada5d1af2f468087b1893f240bd9cae52
ffon <poty1900@hotmail.com>
2017-06-29 14:35:00 +0700

    Update pushMsg2.php

--- pushMsg2.php
+++ pushMsg2.php
@@ -8,8 +8,6 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-
     <style>
         .head-form h1 {
             padding-top: 30px;
@@ -26,24 +24,29 @@
         }
     </style>
 </head>
-
 <body>
-
-
     <div class="container">
         <div class="row">
             <div class="col-xs-12 head-form">
                 <h1 align = "center">Push Massages</h1>
             </div>
             
-
+            
+             <!-- token -->
+                <form methot="POST"> 
+                     <label>Line@</label>
+                    <div class="form-group" align="center">
+                        <button type="radio" class="btn btn-primary" value="QQ4FDBydERg5R34tFiff7M+OOuRNzYKDA/btJh4Whsgl0ztKiDparY2v3TyaoL1LQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctP74gTqe5/G/kLHS2Ixe3w0jsLIaN0guHlHI+3q9c9ZQdB04t89/1O/w1cDnyilFU=" name="tokenLine"> Line@ffon</button>
+                        <button type="radio" class="btn btn-primary" value="xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=" name="tokenLine"> Line@oil</button>
+                    </div>
+            </form>
             <div class="col-md-8 col-md-offset-2">
                 <form method="post">
                     <div class="form-group">
-                        
                         <label>Text</label>
                         <textarea class="form-control" rows="8" id="textArea" name="textArea"></textarea>
                     </div>
+                    
                     <!--buttonMember-->
                     <div class="form-group" align="center">
                         <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal" style="margin-top:30px;margin-bottom:20px;">
@@ -51,37 +54,30 @@
                         </button>
                     </div>
                     <!--Modal-->
-    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-          <form method="post">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="myModalLabel">Member</h4>
-                </div>
-                <div class="container">
-                  
-                        <div class="checkbox">
-                            <label><input type="checkbox" value="Ub5fea2ff169cba24b2179fd33e59e454" name="mid[0]">oil</label>
-                        </div>
-                        <div class="checkbox">
-                            <label><input type="checkbox" value="U7de80d0a2ceea863e831375badd2eb55" name="mid[1]">ffon</label>
-                        </div>
-                </div>
-                
-                <form methot="POST"> <div class="form-group" align="center">
-           <button type="button" class="btn btn-primary" value="QQ4FDBydERg5R34tFiff7M+OOuRNzYKDA/btJh4Whsgl0ztKiDparY2v3TyaoL1LQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctP74gTqe5/G/kLHS2Ixe3w0jsLIaN0guHlHI+3q9c9ZQdB04t89/1O/w1cDnyilFU=" name="tokenLine"> Line@ffon</button>
-           <button type="button" class="btn btn-primary" value="xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=" name="tokenLine"> Line@oil</button>
-            </div></form>
-                
-                <div class="modal-footer">
-                    <button type="submit" class="btn btn-primary" name="submit">Summit</button>
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                </div>
-            </div>
-        </div>
-              </form>
-    </div>
+                    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                        <form method="post">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                        <h4 class="modal-title" id="myModalLabel">Member</h4>
+                                    </div>
+                                    <div class="container">
+                                        <div class="checkbox">
+                                            <label><input type="checkbox" value="Ub5fea2ff169cba24b2179fd33e59e454" name="mid[0]">oil</label>
+                                        </div>
+                                        <div class="checkbox">
+                                            <label><input type="checkbox" value="U7de80d0a2ceea863e831375badd2eb55" name="mid[1]">ffon</label>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="submit" class="btn btn-primary" name="submit">Summit</button>
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
 
                     <!--submitCancel-->
                     <div class="button-sc text-center">
@@ -92,9 +88,6 @@
             </div>
         </div>
     </div>
-
-
-
 
     <script>
         $('#myModal').on('shown.bs.modal', function () {
@@ -108,10 +101,10 @@
 $proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
 $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80'; 
 //$strAccessToken = 'QQ4FDBydERg5R34tFiff7M+OOuRNzYKDA/btJh4Whsgl0ztKiDparY2v3TyaoL1LQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctP74gTqe5/G/kLHS2Ixe3w0jsLIaN0guHlHI+3q9c9ZQdB04t89/1O/w1cDnyilFU=';            
-$strAccessToken = $_POST['tokenLine'];
+
 $text = $_POST['textArea'];
 $midUser = $_POST['mid'];  
-//$tokenLine = $tokenLine;
+$strAccessToken = $_POST['tokenLine'];
 var_dump($strAccessToken) ;
     
 
