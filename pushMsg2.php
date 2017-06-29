50d7c398d03d5a8e5ff6c5b37000efe712cbbda7
ffon <poty1900@hotmail.com>
2017-06-29 15:33:55 +0700

    Update pushMsg2.php

--- pushMsg2.php
+++ pushMsg2.php
@@ -34,15 +34,16 @@
             
              <!-- token -->
                 <form methot="POST"> 
-                    <div class="form-group" align="center">
+                  
+            </form>
+            <div class="col-md-8 col-md-offset-2">
+                <form method="post">
+                      <div class="form-group" align="center">
                        <label>Line@</label><br>
                         <button type="radio" class="btn btn-primary btn-lg"  value="QQ4FDBydERg5R34tFiff7M+OOuRNzYKDA/btJh4Whsgl0ztKiDparY2v3TyaoL1LQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctP74gTqe5/G/kLHS2Ixe3w0jsLIaN0guHlHI+3q9c9ZQdB04t89/1O/w1cDnyilFU=" name="tokenLine"> Line@ffon</button>
                         <button type="radio" class="btn btn-primary btn-lg"  value="xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=" name="tokenLine"> Line@oil</button>
                          
                     </div>
-            </form>
-            <div class="col-md-8 col-md-offset-2">
-                <form method="post">
                     <div class="form-group">
                         <label>Text</label>
                         <textarea class="form-control" rows="8" id="textArea" name="textArea"></textarea>
