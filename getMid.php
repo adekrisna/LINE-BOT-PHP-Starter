7a40089d1dd628d41203a32a1929032bfced475d
ffon <poty1900@hotmail.com>
2017-07-13 10:57:15 +0700

    Update getMid.php

--- getMid.php
+++ getMid.php
@@ -7,11 +7,13 @@ $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
 $strUrl = "https://api.line.me/v2/bot/profile/{userId}";
 
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+$header = array(
+'Content-Type: application/json',
+'Authorization: Bearer ' . $strAccessToken
+);
 
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('f9/uoIUNEP1kL2paNPKAH+EGLrCz2VYyDLRzADLiG6cUM838OEmvwuLDaHOX8Y8gQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBcssXN77lyH4cRgzSRe+ubJT6jlMGO8SmAXXZaS0FNIeAQdB04t89/1O/w1cDnyilFU=');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($strAccessToken);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '2016f3f7fb001c7f38154a3fe3f3202c']);
 $response = $bot->getProfile('U7de80d0a2ceea863e831375badd2eb55');
 if ($response->isSucceeded()) {
@@ -21,10 +23,10 @@ if ($response->isSucceeded()) {
     echo $profile['statusMessage'];
 }
 
-$ch = curl_init( $strUrl);
+$ch = curl_init($strUrl);
 curl_setopt($ch, CURLOPT_GET, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_PROXY, $proxy);
@@ -34,3 +36,4 @@ curl_close ($ch);
 echo "Ok<br>";
 var_dump($response);
 var_dump($httpClient);
+var_dump($result);
