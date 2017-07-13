d9c8fa47bbdf04543ac0274dd25f32ed714b1dbf
ffon <poty1900@hotmail.com>
2017-07-13 12:21:52 +0700

    Update QR_Code.php

--- QR_Code.php
+++ QR_Code.php
@@ -21,9 +21,6 @@
         $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
 
         $get_mid =  $arrJson['events'][0]['source']['userId'];
-        if($get_mid != NULL && $get_mid != ""){
-        $_SESSION['mid'] = $get_mid;
-        }
         if ($arrJson['events'][0]['message']['text'] == "สวัสดี") {
             $arrPostData = array();
             $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
