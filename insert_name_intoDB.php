a0d2b475d1b69e4ced878b49b9e447b818856a48
ffon <poty1900@hotmail.com>
2017-07-14 18:20:37 +0700

    Update insert_name_intoDB.php

--- insert_name_intoDB.php
+++ insert_name_intoDB.php
@@ -11,30 +11,30 @@
 
 
 <?php
-function get_name($mid = null)
-{
-    $proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
-    $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
-    $strAccessToken = "f9/uoIUNEP1kL2paNPKAH+EGLrCz2VYyDLRzADLiG6cUM838OEmvwuLDaHOX8Y8gQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBcssXN77lyH4cRgzSRe+ubJT6jlMGO8SmAXXZaS0FNIeAQdB04t89/1O/w1cDnyilFU=";
-    $content = file_get_contents('php://input');
-    $arrJson = json_decode($content, true);
-    $strUrl = "https://api.line.me/v2/bot/profile/$mid";
-    $header = array(
-    'Content-Type: application/json',
-    'Authorization: Bearer ' . $strAccessToken
-    );
+// function get_name($mid = null)
+// {
+//     $proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
+//     $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
+//     $strAccessToken = "f9/uoIUNEP1kL2paNPKAH+EGLrCz2VYyDLRzADLiG6cUM838OEmvwuLDaHOX8Y8gQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBcssXN77lyH4cRgzSRe+ubJT6jlMGO8SmAXXZaS0FNIeAQdB04t89/1O/w1cDnyilFU=";
+//     $content = file_get_contents('php://input');
+//     $arrJson = json_decode($content, true);
+//     $strUrl = "https://api.line.me/v2/bot/profile/$mid";
+//     $header = array(
+//     'Content-Type: application/json',
+//     'Authorization: Bearer ' . $strAccessToken
+//     );
 
-    $chAdd = curl_init();
-    curl_setopt($chAdd, CURLOPT_URL, $strUrl);
-    curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
-    curl_setopt($chAdd, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($chAdd, CURLOPT_HTTPHEADER, $header);
-    $result = curl_exec($chAdd);
-    $err    = curl_error($chAdd);
-    curl_close($chAdd);
+//     $chAdd = curl_init();
+//     curl_setopt($chAdd, CURLOPT_URL, $strUrl);
+//     curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
+//     curl_setopt($chAdd, CURLOPT_RETURNTRANSFER, true);
+//     curl_setopt($chAdd, CURLOPT_HTTPHEADER, $header);
+//     $result = curl_exec($chAdd);
+//     $err    = curl_error($chAdd);
+//     curl_close($chAdd);
 
-    return $result;
-}
+//     return $result;
+// }
 
 function reply_get_mid()
 {
