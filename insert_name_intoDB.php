b907b0ca7fe662797ef18f71ade3cf3731d3f89c
ffon <poty1900@hotmail.com>
2017-07-14 17:15:20 +0700

    Update insert_name_intoDB.php

--- insert_name_intoDB.php
+++ insert_name_intoDB.php
@@ -1,51 +1,74 @@
 <html>
-    8
     <meta charset="utf-8">
+    99
+<title>@ME</title>
+
+<h1 align = 'center'>@ME</h1>
+    <P align=center>
+        <img src="http://qr-official.line.me/L/oUypr1a-r8.png">
+    </P>
+    <div align=center>line@ffon</div>
+
+
 <?php
+function qr_code()
+{
+    require_once('getName.php');
+
     $proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
     $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
     $strAccessToken = "f9/uoIUNEP1kL2paNPKAH+EGLrCz2VYyDLRzADLiG6cUM838OEmvwuLDaHOX8Y8gQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBcssXN77lyH4cRgzSRe+ubJT6jlMGO8SmAXXZaS0FNIeAQdB04t89/1O/w1cDnyilFU=";
-    
+        
     $content = file_get_contents('php://input');
     $arrJson = json_decode($content, true);
-    $strUrl = "https://api.line.me/v2/bot/profile/U7de80d0a2ceea863e831375badd2eb55";
-    $header = array(
-    'Content-Type: application/json',
-    'Authorization: Bearer ' . $strAccessToken
-    );
-
+    $strUrl = "https://api.line.me/v2/bot/message/reply";
+    
+    $arrHeader = array();
+    $arrHeader[] = "Content-Type: application/json";
+    $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+    $arrPostData = array();
+    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    $arrPostData['messages'][0]['type'] = "text";
+    $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
+    $get_mid =  $arrJson['events'][0]['source']['userId'];
+        
+    if ($arrJson['events'][0]['message']['text'] == "สวัสดี") {
+        $arrPostData = array();
+        $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+        $arrPostData['messages'][0]['type'] = "text";
+        $arrPostData['messages'][0]['text'] = "สวัสดี ".$arrJson['events'][0]['source']['userId'];
+           
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/testem?mid='.$name.'&addby=ffon2');
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        "Content-Type: application/json",
+                                                    )
+         );
+        $result = curl_exec($ch);
+        $err    = curl_error($ch);
+        curl_close($ch);
+    }
+        
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $strUrl);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER,$header);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_PROXY, $proxy);
     curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-    $result = curl_exec($ch);
-    $err    = curl_error($ch);
-    curl_close($ch);
-    var_dump($result)."<br>";
-    $result_decode = json_decode($result);
-    var_dump($result_decode);
-
-    $name = $result_decode->displayName;
-    echo "<br>".$result_decode->userId;
-    echo "<br>".$result_decode->pictureUrl;
-    echo "<br>".$result_decode->statusMessage; 
-    
-    $ch_add = curl_init();
-    curl_setopt($ch_add, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/testem?mid='.$name.'&addby=ffon2');
-    curl_setopt($ch_add, CURLOPT_CUSTOMREQUEST, 'GET');
-    curl_setopt($ch_add, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch_add, CURLOPT_HTTPHEADER, array(
-        "Content-Type: application/json",
-                                                )
-        );
-    curl_setopt($ch_add, CURLOPT_PROXY, $proxy);
-    curl_setopt($ch_add, CURLOPT_PROXYUSERPWD, $proxyauth);
-    $results = curl_exec($ch_add);
-    $err    = curl_error($ch_add);
-    curl_close($ch_add);
+    $results = curl_exec($ch);
+    curl_close ($ch);
     var_dump($results);
+}
+    qr_code();
+    
+            
+    
     ?>
+    
     </html>
