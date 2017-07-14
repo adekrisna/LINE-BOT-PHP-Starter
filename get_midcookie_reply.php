baf00fcba79c88bbdc91378ee6d7f91c8fad0e14
ffon <poty1900@hotmail.com>
2017-07-14 10:07:22 +0700

    Update get_midcookie_reply.php

--- get_midcookie_reply.php
+++ get_midcookie_reply.php
@@ -1,12 +1,13 @@
 <html>
-8
+    9
 <title>@ME</title>
 
 <h1 align = 'center'>@ME</h1>
-<P align=center>
-<img src="http://qr-official.line.me/L/oUypr1a-r8.png">
-</P>
-<div align=center>line@ffon</div>
+    <P align=center>
+        <img src="http://qr-official.line.me/L/oUypr1a-r8.png">
+    </P>
+    <div align=center>line@ffon</div>
+
 
 <?php
 function qr_code()
@@ -14,11 +15,11 @@ function qr_code()
     $proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
     $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
     $strAccessToken = "f9/uoIUNEP1kL2paNPKAH+EGLrCz2VYyDLRzADLiG6cUM838OEmvwuLDaHOX8Y8gQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBcssXN77lyH4cRgzSRe+ubJT6jlMGO8SmAXXZaS0FNIeAQdB04t89/1O/w1cDnyilFU=";
-
+        
     $content = file_get_contents('php://input');
     $arrJson = json_decode($content, true);
     $strUrl = "https://api.line.me/v2/bot/message/reply";
-
+    
     $arrHeader = array();
     $arrHeader[] = "Content-Type: application/json";
     $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
@@ -28,19 +29,38 @@ function qr_code()
     $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
     $get_mid =  $arrJson['events'][0]['source']['userId'];
 
+    $ch_add = curl_init();
+    curl_setopt($ch_add, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/testem?mid='.$get_mid.'&addby=ffon');
+    curl_setopt($ch_add, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch_add, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch_add, CURLOPT_HTTPHEADER, array(
+    "Content-Type: application/json",
+                                            )
+    );
+    $result = curl_exec($ch_add);
+    $err    = curl_error($ch_add);
+    curl_close($ch_add);
+        
+        
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/testem?mid='.$get_mid.'&addby=ffon');
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch, CURLOPT_URL, $strUrl);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        "Content-Type: application/json",
-    ));
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_PROXY, $proxy);
+    curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
     $result = curl_exec($ch);
-    $err    = curl_error($ch);
-    curl_close($ch);
+    echo "Ok<br>";
+    var_dump($_SESSION['mid']);
+    curl_close ($ch);
 }
-qr_code();  
-
-?>
-
+    qr_code();
+    
+            
+    
+    ?>
+    
 </html>
