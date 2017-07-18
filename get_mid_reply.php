5f6d3ee6382db3078f6eea3458eef1545fee87ce
ffon <poty1900@hotmail.com>
2017-07-18 11:27:21 +0700

    Update get_mid_reply.php

--- get_mid_reply.php
+++ get_mid_reply.php
@@ -28,20 +28,24 @@
         $arrPostData['messages'][0]['type'] = "text";
         $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
         $get_mid =  $arrJson['events'][0]['source']['userId'];
+        echo "get_mid";
+        var_dump($get_mid);
         
         $chAdd = curl_init();
         curl_setopt($chAdd,CURLOPT_URL, 'http://uat.dxplace.com/dxtms/testem?mid='.$get_mid.'&addby=ffon');
         curl_setopt($chAdd,CURLOPT_CUSTOMREQUEST , 'GET');
-        curl_setopt($chAdd,CURLOPT_RETURNTRANSFER , true);
+        //curl_setopt($chAdd,CURLOPT_RETURNTRANSFER , true);
         curl_setopt($chAdd, CURLOPT_HTTPHEADER, array(
         "Content-Type: application/json",
                                                 )
         );
-        $result = curl_exec($chAdd);
+        $results = curl_exec($chAdd);
         $err    = curl_error($chAdd);
         curl_close($chAdd);
+        echo "result dx";
+        var_dump($results);
         
-        if ($arrJson['events'][0]['message']['text'] == "สวัสดี") {
+        if ($arrJson['events'][0]['message']['text'] == "a") {
             $arrPostData = array();
             $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
             $arrPostData['messages'][0]['type'] = "text";
@@ -54,12 +58,15 @@
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        //curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_PROXY, $proxy);
         curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
         $result = curl_exec($ch);
         curl_close ($ch);
+        
+        echo "result";
+        var_dump($result);
         
     }
     qr_code();
