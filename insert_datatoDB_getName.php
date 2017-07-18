ecd2748bfb3da530b94d7625261b5cbcff888d94
ffon <poty1900@hotmail.com>
2017-07-18 15:47:32 +0700

    Update insert_datatoDB_getName.php

--- insert_datatoDB_getName.php
+++ insert_datatoDB_getName.php
@@ -1,12 +1,13 @@
 <?php
-function get_Name($mid)
+
+function get_Name()
 {
     $proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
     $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
     $strAccessToken = "f9/uoIUNEP1kL2paNPKAH+EGLrCz2VYyDLRzADLiG6cUM838OEmvwuLDaHOX8Y8gQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBcssXN77lyH4cRgzSRe+ubJT6jlMGO8SmAXXZaS0FNIeAQdB04t89/1O/w1cDnyilFU=";
     $content = file_get_contents('php://input');
     $arrJson = json_decode($content, true);
-    $strUrl = "https://api.line.me/v2/bot/profile/$mid";
+    $strUrl = "https://api.line.me/v2/bot/profile/U8c4eb5ebbd3493b74c6d17a77d3e6cd3";
     $header = array(
     'Content-Type: application/json',
     'Authorization: Bearer ' . $strAccessToken
@@ -25,9 +26,7 @@ function get_Name($mid)
     
     insert_to_tb($result);
 }
-
-
-function insert_data_tb($data)
+function insert_to_tb($data)
 {
     $result_decode = json_decode($data);
     var_dump($result_decode);
@@ -54,48 +53,4 @@ function insert_data_tb($data)
     echo "result return : ";
     var_dump($result);
 }
-
-function get_mid()
-{
-    $strAccessToken = "XDEio/U+1tLcglINRntoiWnm3xBRzApRnLm5FHhpqHGEtU21j01yqjlxr83equ5W6qVYXGI80LOObJe1H9EaoK4ZfSiSHwpUrRgQxlREc/aSZQavLqwyHsT1rDcxjzf9ekwtwN1VXkZsCGo9bRxI5AdB04t89/1O/w1cDnyilFU=";
- 
-    $content = file_get_contents('php://input');
-    $arrJson = json_decode($content, true);
- 
-    $strUrl = "https://api.line.me/v2/bot/message/reply";
- 
-    $arrHeader = array();
-    $arrHeader[] = "Content-Type: application/json";
-    $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
-
-    $arrPostData = array();
-    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-    $arrPostData['messages'][0]['type'] = "text";
-    $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
-  
- 
-    if ($arrJson['events'][0]['message']['text'] == "a") {
-          $arrPostData = array();
-          $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-          $arrPostData['messages'][0]['type'] = "text";
-          $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
-         $get_mid =  $arrJson['events'][0]['source']['userId'];
-
-         get_name($get_mid);
-    }
- 
- 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $strUrl);
-    curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    $result = curl_exec($ch);
-    curl_close ($ch);
-
-    var_dump($result);
-    var_dump($get_mid);
-}
+get_name();
