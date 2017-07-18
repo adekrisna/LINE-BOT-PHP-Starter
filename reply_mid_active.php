e99f52486c57e4579314c4da799cd561062391b7
ffon <poty1900@hotmail.com>
2017-07-18 16:09:49 +0700

    Update reply_mid_active.php

--- reply_mid_active.php
+++ reply_mid_active.php
@@ -1,4 +1,34 @@
 <?php
+function get_name(){
+    $proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
+    $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
+    $strAccessToken = "XDEio/U+1tLcglINRntoiWnm3xBRzApRnLm5FHhpqHGEtU21j01yqjlxr83equ5W6qVYXGI80LOObJe1H9EaoK4ZfSiSHwpUrRgQxlREc/aSZQavLqwyHsT1rDcxjzf9ekwtwN1VXkZsCGo9bRxI5AdB04t89/1O/w1cDnyilFU="; $content = file_get_contents('php://input');
+    $arrJson = json_decode($content, true);
+    $strUrl = "https://api.line.me/v2/bot/profile/U7de80d0a2ceea863e831375badd2eb55";
+    $header = array(
+    'Content-Type: application/json',
+    'Authorization: Bearer ' . $strAccessToken
+    );
+    $chAdd = curl_init();
+    curl_setopt($chAdd, CURLOPT_URL, $strUrl);
+    curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($chAdd, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($chAdd, CURLOPT_HTTPHEADER,$header);
+    curl_setopt($ch, CURLOPT_PROXY, $proxy);
+    curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+    $result = curl_exec($chAdd);
+    $err    = curl_error($chAdd);
+    curl_close($chAdd);
+    var_dump($result)."<br>";
+    $result_decode = json_decode($result);
+    var_dump($result_decode);
+    
+    $name = $result_decode->displayName;
+    var_dump($name);
+    echo "<br>".$result_decode->userId;
+    echo "<br>".$result_decode->pictureUrl;
+    echo "<br>".$result_decode->statusMessage; 
+}
 function insert_data_tb($mid){
     $chAdd = curl_init();
           curl_setopt($chAdd, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/line_member?mid='.$mid.'&line_name=ffon_test5'.'&image=image'.'&add_by=1');
@@ -15,6 +45,8 @@ function insert_data_tb($mid){
 
 function get_mid()
 {
+    $proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
+    $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
     $strAccessToken = "XDEio/U+1tLcglINRntoiWnm3xBRzApRnLm5FHhpqHGEtU21j01yqjlxr83equ5W6qVYXGI80LOObJe1H9EaoK4ZfSiSHwpUrRgQxlREc/aSZQavLqwyHsT1rDcxjzf9ekwtwN1VXkZsCGo9bRxI5AdB04t89/1O/w1cDnyilFU=";
  
     $content = file_get_contents('php://input');
@@ -51,6 +83,8 @@ function get_mid()
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_PROXY, $proxy);
+    curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
     $result = curl_exec($ch);
     curl_close ($ch);
 
@@ -58,7 +92,7 @@ function get_mid()
     var_dump($get_mid);
 
 }
-
+get_name();
 get_mid();
 
 ?>
