11802f82094aaaa3edb1f620ccdc88eb888e197b
ffon <poty1900@hotmail.com>
2017-07-18 10:28:25 +0700

    Update insert_data_intoDB.php

--- insert_data_intoDB.php
+++ insert_data_intoDB.php
@@ -1,6 +1,6 @@
 <html>
     <meta charset="utf-8">
-    99
+    6
 <title>@ME</title>
 
 <h1 align = 'center'>@ME</h1>
@@ -36,6 +36,7 @@ function get_name($mid = null)
     return $result;
 }
 
+
 function reply_get_mid()
 {
         
@@ -56,50 +57,51 @@ function reply_get_mid()
         $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
         $get_mid =  $arrJson['events'][0]['source']['userId'];
 
-
-   
     
-    if ($arrJson['events'][0]['message']['text'] == "สวัสดี") {
-        $arrPostData = array();
-        $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-        $arrPostData['messages'][0]['type'] = "text";
-        $arrPostData['messages'][0]['text'] = "สวัสดี ".$arrJson['events'][0]['source']['userId'];
-        if ($get_mid!=null) {
-            $userObj = get_name($get_mid);
-            $userObj_decode = json_decode($userObj);
+    // if ($arrJson['events'][0]['message']['text'] == "สวัสดี") {
+    //     $arrPostData = array();
+    //     $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    //     $arrPostData['messages'][0]['type'] = "text";
+    //     $arrPostData['messages'][0]['text'] = "สวัสดี ".$arrJson['events'][0]['source']['userId'];
+    //     if ($get_mid!=null) {
+    //         $userObj = get_name($get_mid);
+    //         $userObj_decode = json_decode($userObj);
 
-            $name = $userObj_decode->displayName;
-            $image = $userObj_decode->pictureUrl;
-            $chAdd = curl_init();
-            curl_setopt($chAdd, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/line_member?mid='.$get_mid.'&line_name='.$name.'&image='.$image.'&add_by=ffon3');
-            curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
-            curl_setopt($chAdd, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($chAdd, CURLOPT_HTTPHEADER, array(
-            "Content-Type: application/json",
-                                        )
-            );
-            $result = curl_exec($chAdd);
-            $err    = curl_error($chAdd);
-            curl_close($chAdd);
-            var_dump($result);
-        }
-    }
+    //         $name = $userObj_decode->displayName;
+    //         $image = $userObj_decode->pictureUrl;
+
+    //         $chAdd = curl_init();
+    //         curl_setopt($chAdd, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/line_member?mid='.$get_mid.'&line_name='.$name.'&image='.$image.'&addby=ffon3');
+    //         curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
+    //         curl_setopt($chAdd, CURLOPT_RETURNTRANSFER, true);
+    //         curl_setopt($chAdd, CURLOPT_HTTPHEADER, array(
+    //         "Content-Type: application/json",
+    //                                     )
+    //         );
+    //         $result = curl_exec($chAdd);
+    //         $err    = curl_error($chAdd);
+    //         curl_close($chAdd);
+    //         var_dump($result);
+    //     }
+    // }
         
         
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $strUrl);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_PROXY, $proxy);
-        curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-        $result = curl_exec($ch);
-        curl_close ($ch);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $strUrl);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_PROXY, $proxy);
+    curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+    $result = curl_exec($ch);
+    curl_close ($ch);
 }
     reply_get_mid();
+
+
     ?>
     
     </html>
