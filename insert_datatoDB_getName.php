c06b445fcf6652f9b0a0721ab8b053f7f9a06c30
ffon <poty1900@hotmail.com>
2017-07-18 15:45:52 +0700

    Update insert_datatoDB_getName.php

--- insert_datatoDB_getName.php
+++ insert_datatoDB_getName.php
@@ -1,18 +1,16 @@
 <?php
-
-function get_Name()
+function get_Name($mid)
 {
     $proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
     $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
     $strAccessToken = "f9/uoIUNEP1kL2paNPKAH+EGLrCz2VYyDLRzADLiG6cUM838OEmvwuLDaHOX8Y8gQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBcssXN77lyH4cRgzSRe+ubJT6jlMGO8SmAXXZaS0FNIeAQdB04t89/1O/w1cDnyilFU=";
     $content = file_get_contents('php://input');
     $arrJson = json_decode($content, true);
-    $strUrl = "https://api.line.me/v2/bot/profile/U8c4eb5ebbd3493b74c6d17a77d3e6cd3";
+    $strUrl = "https://api.line.me/v2/bot/profile/$mid";
     $header = array(
     'Content-Type: application/json',
     'Authorization: Bearer ' . $strAccessToken
     );
-
     $chAdd = curl_init();
     curl_setopt($chAdd, CURLOPT_URL, $strUrl);
     curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
@@ -24,18 +22,12 @@ function get_Name()
     $err    = curl_error($chAdd);
     curl_close($chAdd);
     var_dump($result)."<br>";
-    // $result_decode = json_decode($result);
-    // var_dump($result_decode);
     
-    // $name = $result_decode->displayName;
-    // var_dump($name);
-    // echo "<br>".$result_decode->userId;
-    // echo "<br>".$result_decode->pictureUrl;
-    // echo "<br>".$result_decode->statusMessage;
     insert_to_tb($result);
 }
 
-function insert_to_tb($data)
+
+function insert_data_tb($data)
 {
     $result_decode = json_decode($data);
     var_dump($result_decode);
@@ -48,7 +40,6 @@ function insert_to_tb($data)
     $image=$result_decode->pictureUrl;
     echo "<br>";
     $status=$result_decode->statusMessage;
-
     $chAdd = curl_init();
     curl_setopt($chAdd, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/line_member?mid='.$mid.'&line_name='.$name.'&image='.$image.'&add_by=1');
     curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
@@ -63,4 +54,48 @@ function insert_to_tb($data)
     echo "result return : ";
     var_dump($result);
 }
-get_name();
+
+function get_mid()
+{
+    $strAccessToken = "XDEio/U+1tLcglINRntoiWnm3xBRzApRnLm5FHhpqHGEtU21j01yqjlxr83equ5W6qVYXGI80LOObJe1H9EaoK4ZfSiSHwpUrRgQxlREc/aSZQavLqwyHsT1rDcxjzf9ekwtwN1VXkZsCGo9bRxI5AdB04t89/1O/w1cDnyilFU=";
+ 
+    $content = file_get_contents('php://input');
+    $arrJson = json_decode($content, true);
+ 
+    $strUrl = "https://api.line.me/v2/bot/message/reply";
+ 
+    $arrHeader = array();
+    $arrHeader[] = "Content-Type: application/json";
+    $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+
+    $arrPostData = array();
+    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    $arrPostData['messages'][0]['type'] = "text";
+    $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
+  
+ 
+    if ($arrJson['events'][0]['message']['text'] == "a") {
+          $arrPostData = array();
+          $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+          $arrPostData['messages'][0]['type'] = "text";
+          $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
+         $get_mid =  $arrJson['events'][0]['source']['userId'];
+
+         get_name($get_mid);
+    }
+ 
+ 
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $strUrl);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $result = curl_exec($ch);
+    curl_close ($ch);
+
+    var_dump($result);
+    var_dump($get_mid);
+}
