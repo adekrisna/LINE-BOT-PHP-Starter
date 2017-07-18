8a3bc20e9ea50d562bfbd835dc001429052cbd94
ffon <poty1900@hotmail.com>
2017-07-18 17:03:12 +0700

    Update reply_mid_active.php

--- reply_mid_active.php
+++ reply_mid_active.php
@@ -1,7 +1,34 @@
 <?php
 get_mid();
 
-function insert_data_tb($mid){
+function get_Name($mid)
+{
+    $proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
+    $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
+    $strAccessToken = "f9/uoIUNEP1kL2paNPKAH+EGLrCz2VYyDLRzADLiG6cUM838OEmvwuLDaHOX8Y8gQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBcssXN77lyH4cRgzSRe+ubJT6jlMGO8SmAXXZaS0FNIeAQdB04t89/1O/w1cDnyilFU=";
+    $content = file_get_contents('php://input');
+    $arrJson = json_decode($content, true);
+    $strUrl = "https://api.line.me/v2/bot/profile/$mid";
+    $header = array(
+    'Content-Type: application/json',
+    'Authorization: Bearer ' . $strAccessToken
+    );
+    $chAdd = curl_init();
+    curl_setopt($chAdd, CURLOPT_URL, $strUrl);
+    curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($chAdd, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($chAdd, CURLOPT_HTTPHEADER, $header);
+    curl_setopt($ch, CURLOPT_PROXY, $proxy);
+    curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+    $result = curl_exec($chAdd);
+    $err    = curl_error($chAdd);
+    curl_close($chAdd);
+    echo "result get name";
+    var_dump($result)."<br>";
+}
+
+function insert_data_tb($mid)
+{
     $chAdd = curl_init();
           curl_setopt($chAdd, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/line_member?mid='.$mid.'&line_name=ffon_tes8'.'&image=image'.'&add_by=1');
           curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
@@ -44,6 +71,7 @@ function get_mid()
          $get_mid =  $arrJson['events'][0]['source']['userId'];
 
          insert_data_tb($get_mid);
+         get_name($get_mid);
     }
  
  
@@ -62,7 +90,4 @@ function get_mid()
 
     var_dump($result);
     var_dump($get_mid);
-
 }
-
-?>
