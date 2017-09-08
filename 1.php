c6eb35131ccbfbc1af7690e84af1ab9080b5475f
ffon <poty1900@hotmail.com>
2017-09-08 11:10:20 +0700

    edit token and proxy

--- 1.php
+++ 1.php
@@ -15,9 +15,9 @@
 function reply_get_mid()
 {
         
-        $proxy = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
-        $proxyauth = 'http://fixie:f15Ug5dvUX8MX7F@velodrome.usefixie.com:80';
-        $strAccessToken = "f9/uoIUNEP1kL2paNPKAH+EGLrCz2VYyDLRzADLiG6cUM838OEmvwuLDaHOX8Y8gQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBcssXN77lyH4cRgzSRe+ubJT6jlMGO8SmAXXZaS0FNIeAQdB04t89/1O/w1cDnyilFU=";
+        $proxy = 'if u want to fix url';
+        $proxyauth = 'if u want to fix url';
+        $strAccessToken = "Token";
         
         $content = file_get_contents('php://input');
         $arrJson = json_decode($content, true);
@@ -31,29 +31,7 @@ function reply_get_mid()
         $arrPostData['messages'][0]['type'] = "text";
         $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
         $get_mid =  $arrJson['events'][0]['source']['userId'];
-
-    // if ($arrJson['events'][0]['message']['text'] == "a") {
-    //     $arrPostData = array();
-    //     $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-    //     $arrPostData['messages'][0]['type'] = "text";
-    //     $arrPostData['messages'][0]['text'] = "สวัสดี ".$arrJson['events'][0]['source']['userId'];
-    //     $get_mid =  $arrJson['events'][0]['source']['userId'];
-        
-    //     $chAdd = curl_init();
-    //     curl_setopt($chAdd, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/line_member?mid='.$_get_mid.'&line_name=ffon_test'.'&image=image'.'&add_by=ffon3');
-    //     curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
-    // // curl_setopt($chAdd, CURLOPT_RETURNTRANSFER, true);
-    //     curl_setopt($chAdd, CURLOPT_HTTPHEADER, array(
-    //     "Content-Type: application/json",
-    //                             )
-    //     );
-    //     $result = curl_exec($chAdd);
-    //     $err    = curl_error($chAdd);
-    //     curl_close($chAdd);
-    //     echo "result return : ";
-    //     var_dump($result);
-    // }
-        
+   
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $strUrl);
     curl_setopt($ch, CURLOPT_HEADER, false);
